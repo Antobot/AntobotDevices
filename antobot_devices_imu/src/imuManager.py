620f65caf41879dcfdb1ca969fad7604f56fc611
zhuang-antobot <zhuang.zhou@antobot.ai>
2025-02-13 14:06:16 +0000

    cleaned up and tested imuManager

--- antobot_devices_imu/src/imuManager.py
+++ antobot_devices_imu/src/imuManager.py
@@ -24,11 +24,7 @@ class imuManager():
         param_file = paramPath+ "/param/xsens_mti_node.yaml"
 
         imu_config = self.load_config()
-        print("imu_config:\n",imu_config)
-        
-        # Initialise ROS Launcher
-        #self._launch = roslaunch.scriptapi.ROSLaunch()
-        #self._launch.start()
+
 
         for device, settings in imu_config.items():
             switch = settings.get("switch", False)
@@ -37,23 +33,8 @@ class imuManager():
                     device_bno=True
                 if device == "xsens":
                     device_xsens = True
-                    if os.path.isfile(param_file):
-                        rospy.loginfo("Loading parameters from %s", param_file)
-                        xsens_paramlist=rosparam.load_file(param_file, default_namespace="/xsens_mti_node")  #
-                        print(xsens_paramlist)
-                        for params, ns in xsens_paramlist:
-                            rospy.loginfo(f"Loading parameters to namespace: {ns}")
-                            rosparam.upload_params(ns,params)#
-                        #xsens_param_file=rospy.get_param('~raram_file','$(find xsens_mti_driver)/param/xsens_mti_node.yaml')
-                        #rospy.loginfo(f"Loading parameters from: {xsens_param_file}")
-                        #rospy.set_param('imu_xsens',rospy.get_param('',rospy.load_param(xsens_param_file)))
-        #rospy.sleep(4)
-        if rospy.has_param("/xsens_mti_node/port"):
-            rospy.loginfo("parameter port found in param server")
-        else:
-            rospy.logerr("parameter port not found in param server")
+                    
         self.createLauncher(device_bno, device_xsens)
-        #print("launching executable {}".format(exec_name))
 
 
 
@@ -71,84 +52,44 @@ class imuManager():
 
     def createLauncher(self, device_bno, device_xsens):
 
-
-        nodes_to_launch = []
-        
-        #uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        #roslaunch.configure_logging(uuid)
+ 
         launch = roslaunch.scriptapi.ROSLaunch()
         launch.start()
         if device_bno  and not device_xsens :
-                rospy.loginfo("Launching BNO550 IMU ")
-               
-                #launch.start()
-                node_bno = roslaunch.core.Node(package="am_bno055_imu",
+            rospy.loginfo("Launching BNO550 IMU ")
+            node_bno = roslaunch.core.Node(package="am_bno055_imu",
                                            node_type="am_bno055_imu_node",
                                            name="imu_bno",namespace="imu")
-                #nodes_to_launch.append(node)
-                launch.launch(node_bno)
-                #launch = roslaunch.parent.ROSLaunchParent(uuid, nodes_to_launch)
-                
-                #launcher = RoslaunchWrapperObject(uuid, nodes_to_launch)
-                #launcher.start_node_name("imu_bno")
-                #launch.start()
+            launch.launch(node_bno)
 
         elif not device_bno  and  device_xsens :
-           
-
-                rospy.loginfo("Launching Xsens IMU")
-                node_xsens = roslaunch.core.Node(package="xsens_mti_driver",
+            rospy.loginfo("Launching Xsens IMU")
+            node_xsens = roslaunch.core.Node(package="xsens_mti_driver",
                                            node_type="xsens_mti_node",
-                                           name="imu_xsens",namespace="xsens_mti_node",output="screen")
-                #nodes_to_launch.append(node)
-                uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-                roslaunch.configure_logging(uuid)
-                launch = roslaunch.parent.ROSLaunchParent(uuid, ["/root/catkin_ws/src/towerScan/AntoExternal/xsens_ros_mti_driver/launch/xsens_mti_node.launch"])
-                
-                #launcher = RoslaunchWrapperObject(uuid, nodes_to_launch)
-                #launcher.start_node_name("imu_xsens")
-                #launch.launch(node_xsens)
-                launch.start()
+                                           name="imu_xsens",namespace="xsens_mti_node",output="screen")    
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/root/catkin_ws/src/towerScan/AntoExternal/xsens_ros_mti_driver/launch/xsens_mti_node.launch"])
+            launch.start()
 
         elif device_bno  and  device_xsens:
             # When both device are enabled, bno's IMU to remain on the default topic ("imu/data")
-            # and remap the xsens’s topic accordingly.
+            # and rename the xsens’s topic accordingly.
                 
-                rospy.loginfo("Launching BNO's node (default imu/data) and XSENS's node remapped to /imu/data/xsens")
-                node_bno = roslaunch.core.Node(package="am_bno055_imu",
+            rospy.loginfo("Launching BNO's node (bno/imu/data) and XSENS's node  /xsens/imu/data")
+            node_bno = roslaunch.core.Node(package="am_bno055_imu",
                                            node_type="am_bno055_imu_node",
-                                           name="imu_bno",namespace="bno/imu")
-                launch.launch(node_bno)
+                                           name="imu",namespace="bno/imu")
+            launch.launch(node_bno)
                 
-                node_xsens = roslaunch.core.Node(package="xsens_mti_driver",
-                                           node_type="xsens_mti_node",
-                                           name="imu_xsens",
-                                             )
-               
-                
-                uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-                roslaunch.configure_logging(uuid)
-                """
-                launch = roslaunch.parent.ROSLaunchParent(uuid, ["/root/catkin_ws/src/towerScan/AntoExternal/xsens_ros_mti_driver/launch/xsens_mti_node_remap.launch"])
-                """
-                cli_args=['xsens_mti_driver','xsens_mti_node.launch','name_space:=xsens']
-                roslaunch_xsens = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
-                roslaunch_xsens_args = cli_args[2:]
-                launch_files=[(roslaunch_xsens,roslaunch_xsens_args)]
-                parent=roslaunch.parent.ROSLaunchParent(uuid,launch_files)
-               
-                #nodes_to_launch.extend([node_bno, node_xsens])
-                #uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-                #roslaunch.configure_logging(uuid)
-                #launch = roslaunch.parent.ROSLaunchParent(uuid,nodes_to_launch)
-                #launch.start()
-                #launcher = RoslaunchWrapperObject(uuid, nodes_to_launch)
-                #launcher.start_node_name("imu_bno")
-                #launcher.start_node_name("imu_xsens")
-                
-                #launch = roslaunch.parent.ROSLaunchParent(uuid, [node_xsens])
-                
-                parent.start()
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            cli_args=['xsens_mti_driver','xsens_mti_node.launch','name_space:=xsens']
+            roslaunch_xsens = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
+            roslaunch_xsens_args = cli_args[2:]
+            launch_files=[(roslaunch_xsens,roslaunch_xsens_args)]
+            parent=roslaunch.parent.ROSLaunchParent(uuid,launch_files)     
+            parent.start()
                 
             
             
