ddf08e5bcbc312948898ac1ca76f36110d34febb
Zhuang Zhou <zhuang.zhou@antobot.ai>
2025-07-04 09:27:29 +0100

    do not sent invalid data to ntrip server

--- antobot_devices_gps/src/gps_f9p.py
+++ antobot_devices_gps/src/gps_f9p.py
@@ -52,7 +52,7 @@ class F9P_GPS:
         self.method = method
         self.poll_buff = 1
         self.poll_buff_pre =1
-        self.base_station=True
+        self.base_station=False
         if self.dev_type == "urcu":
             self.port = spidev.SpiDev()
         elif self.dev_type == "usb":
@@ -340,12 +340,12 @@ class F9P_GPS:
 
                 #print(self.hAcc)
             if streamed_data.startswith("$GNGGA"):
-                if self.gps_time_offset < 0.5:
-                    self.gga_msg_pub.publish(streamed_data)
                 gga_parse = pynmea2.parse(streamed_data)
                 try:
                     self.gga_gps_qual = int(gga_parse.gps_qual)
                     self.num_sats = int(gga_parse.num_sats)         # Number of satellites
+                    if self.gps_time_offset < 0.5:
+                    self.gga_msg_pub.publish(streamed_data)
                 except:
                     print("GPS_quality value invalid")
                 
