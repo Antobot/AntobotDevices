97980cf5d6470e846bbdcbfca17ee8395d7a5c39
huaide <huaide.wang@nicecart.ai>
2025-09-28 08:39:10 +0100

    feat: update renameSerialPort for Ubuntu22

--- antobot_devices_joy/script/renameSerialPort.sh
+++ antobot_devices_joy/script/renameSerialPort.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-sudo rm -rf /etc/udev/rules.d/usb.rules
+sudo rm -rf /etc/udev/rules.d/99-usb.rules
 
-device=ttyUSB0
+RULE="SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"7523\", MODE=\"0777\", SYMLINK+=\"anto_joy\", GROUP=\"dialout\""
 
-cd ~/
-script -c "udevadm info --attribute-walk --name=/dev/$device" serial_info.log
-
-logfile="$HOME/serial_info.log"
-count=0
-kernel_value=""
-while IFS= read -r line; do
-	if [[ $line =~ KERNELS==\"([^\"]+)\" ]];then
-		((count++))
-		if [ $count -eq 2 ]; then
-			kernel_value="${BASH_REMATCH[1]}"
-			break
-		fi
-	fi
-done < "$logfile" 
-
-RULE="KERNELS==\"$kernel_value\", MODE:=\"0777\", GROUP:=\"dialout\", SYMLINK+=\"anto_joy\""
-
-echo "$RULE" | sudo tee -a /etc/udev/rules.d/usb.rules > /dev/null
+echo "$RULE" | sudo tee -a /etc/udev/rules.d/99-usb.rules > /dev/null
 
 sudo udevadm trigger
 	
-rm "$logfile"
-
-echo '=================================================='
-ls -l /dev | grep ttyUSB
+ls -l /dev | grep tty
 
 exit 0
