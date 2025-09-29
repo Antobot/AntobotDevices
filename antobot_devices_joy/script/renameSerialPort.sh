423f64201c8e1042b53f17c8f33146e26f690342
huaide <huaide.wang@nicecart.ai>
2025-09-29 18:35:02 +0800

    feat: add KERNELS to renameSerialPort

--- antobot_devices_joy/script/renameSerialPort.sh
+++ antobot_devices_joy/script/renameSerialPort.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-sudo rm -rf /etc/udev/rules.d/99-usb.rules
+Device_Name="/dev/ttyUSB0"
+LINK_NAME="anto_gps" # anto_gps / anto_joy
+RULE_FILE="/etc/udev/rules.d/99-usb_$LINK_NAME.rules"
+# echo $RULE_FILE
+sudo rm -rf $RULE_FILE
 
-RULE="SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"7523\", MODE=\"0777\", SYMLINK+=\"anto_joy\", GROUP=\"dialout\""
+KPATH=$(udevadm info -q path -n "$Device_Name" | grep -o 'usb[0-9]\+/\([^/]\+\)' | tail -n1 | cut -d/ -f2)
+# echo $KPATH
 
-echo "$RULE" | sudo tee -a /etc/udev/rules.d/99-usb.rules > /dev/null
+# # CH340： ATTRS{idVendor}=="1a86"； ATTRS{idProduct}=="7523"
+RULE="SUBSYSTEM==\"tty\", KERNELS==\"$KPATH\", ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"7523\", MODE=\"0777\", SYMLINK+=\"$LINK_NAME\", GROUP=\"dialout\""
 
+echo "$RULE" | sudo tee $RULE_FILE > /dev/null
+
+sudo udevadm control --reload-rules
 sudo udevadm trigger
-	
-ls -l /dev | grep tty
 
-exit 0
+
