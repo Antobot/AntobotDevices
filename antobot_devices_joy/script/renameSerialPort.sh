344c85703abb5be8d3d287da8e4f8bfa636d9a2e
huaide <huaide.wang@nicecart.ai>
2025-09-30 02:55:06 +0100

    fix: KERNELS failing on Orin NX

--- antobot_devices_joy/script/renameSerialPort.sh
+++ antobot_devices_joy/script/renameSerialPort.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-Device_Name="/dev/ttyUSB0"
+Device_Name="/dev/ttyUSB0" # ttyCH341USB0 / ttyUSB0
 LINK_NAME="anto_gps" # anto_gps / anto_joy
 RULE_FILE="/etc/udev/rules.d/99-usb_$LINK_NAME.rules"
-# echo $RULE_FILE
+
 sudo rm -rf $RULE_FILE
 
-KPATH=$(udevadm info -q path -n "$Device_Name" | grep -o 'usb[0-9]\+/\([^/]\+\)' | tail -n1 | cut -d/ -f2)
-# echo $KPATH
+BUSHNUM=$(udevadm info -a -n "$Device_Name" | grep 'ATTRS{busnum}' | head -n1 | sed -E 's/.*"([^"]+).*/\1/') 
+echo "busnum:$BUSHNUM"
 
-# # CH340： ATTRS{idVendor}=="1a86"； ATTRS{idProduct}=="7523"
-RULE="SUBSYSTEM==\"tty\", KERNELS==\"$KPATH\", ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"7523\", MODE=\"0777\", SYMLINK+=\"$LINK_NAME\", GROUP=\"dialout\""
+DEVPATH=$(udevadm info -a -n "$Device_Name" | grep 'ATTRS{devpath}' | head -n1 | sed -E 's/.*"([^"]+).*/\1/') 
+echo "devpath:$DEVPATH"
+
+RULE="SUBSYSTEM==\"tty\", ATTRS{busnum}==\"$BUSHNUM\", ATTRS{devpath}==\"$DEVPATH\", MODE=\"0777\", SYMLINK+=\"$LINK_NAME\", GROUP=\"dialout\""
 
 echo "$RULE" | sudo tee $RULE_FILE > /dev/null
 
