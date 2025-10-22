fa531efa5a0c06848c98b39af36e30d9cda2df84
YuChen-cart <yu.chen@nicecart.ai>
2025-10-22 11:52:43 +0800

    double mid360 config

--- antobot_devices_lidar/src/aviaMsg2pointcloud2.cpp
+++ antobot_devices_lidar/src/aviaMsg2pointcloud2.cpp
@@ -9,21 +9,35 @@ class aviaMsgNode : public rclcpp::Node
 {
 public:
     aviaMsgNode() : Node("aviaMsgNode") {
-        m_lidar_sub = this->create_subscription<livox_ros_driver2::msg::CustomMsg>("", 10, std::bind(&aviaMsgNode::lidarCB, this, std::placeholders::_1));
-
         this->declare_parameter("config_path", "");
         std::string config_path;
         this->get_parameter<std::string>("config_path", config_path);
         YAML::Node config = YAML::LoadFile(config_path);
 
-        input_lidar_topic = config["input_lidar_topic"].as<std::string>();
-        output_lidar_topic = config["output_lidar_topic"].as<std::string>();
-        output_lidar_frame = config["output_lidar_frame"].as<std::string>();
+        input_lidar_topic1 = config["input_lidar_topic1"].as<std::string>();
+        input_lidar_topic2 = config["input_lidar_topic2"].as<std::string>();
+        output_lidar_topic1 = config["output_lidar_topic1"].as<std::string>();
+        output_lidar_topic2 = config["output_lidar_topic2"].as<std::string>();
+        output_lidar_frame1 = config["output_lidar_frame1"].as<std::string>();
+        output_lidar_frame2 = config["output_lidar_frame2"].as<std::string>();
+
+        lidar_num = config["lidar_num"].as<int>();
         filter_num = config["filter_num"].as<int>();
+
         lidar_max_range = config["lidar_max_range"].as<double>();
         lidar_min_range = config["lidar_min_range"].as<double>();
 
-        m_lidar_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(output_lidar_topic, 10000);
+        if (lidar_num == 1) {
+            m_lidar_sub_1 = this->create_subscription<livox_ros_driver2::msg::CustomMsg>(input_lidar_topic1, 10, std::bind(&aviaMsgNode::lidarCB_1, this, std::placeholders::_1));
+        }else if (lidar_num == 2) {
+            m_lidar_sub_1 = this->create_subscription<livox_ros_driver2::msg::CustomMsg>(input_lidar_topic1, 10, std::bind(&aviaMsgNode::lidarCB_1, this, std::placeholders::_1));
+            m_lidar_sub_2 = this->create_subscription<livox_ros_driver2::msg::CustomMsg>(input_lidar_topic2, 10, std::bind(&aviaMsgNode::lidarCB_2, this, std::placeholders::_1));
+        }
+
+
+
+        m_lidar_pub_1 = this->create_publisher<sensor_msgs::msg::PointCloud2>(output_lidar_topic1, 10000);
+        m_lidar_pub_2 = this->create_publisher<sensor_msgs::msg::PointCloud2>(output_lidar_topic2, 10000);
 
 
 
@@ -33,7 +47,7 @@ public:
 private:
 
 
-    void lidarCB(const livox_ros_driver2::msg::CustomMsg::SharedPtr msg) {
+    void lidarCB_1(const livox_ros_driver2::msg::CustomMsg::SharedPtr msg) {
 
         pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZINormal>);
         cloud->clear();
@@ -43,10 +57,21 @@ private:
         sensor_msgs::msg::PointCloud2 cloud_msg;
         pcl::toROSMsg(*cloud, cloud_msg);
         cloud_msg.header.stamp = msg->header.stamp;
-        cloud_msg.header.frame_id = output_lidar_frame;
-        m_lidar_pub->publish(cloud_msg);
+        cloud_msg.header.frame_id = output_lidar_frame1;
+        m_lidar_pub_1->publish(cloud_msg);
+    }
+    void lidarCB_2(const livox_ros_driver2::msg::CustomMsg::SharedPtr msg) {
 
+        pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZINormal>);
+        cloud->clear();
 
+        cloud = livox2PCL(msg, filter_num, lidar_min_range, lidar_max_range);
+
+        sensor_msgs::msg::PointCloud2 cloud_msg;
+        pcl::toROSMsg(*cloud, cloud_msg);
+        cloud_msg.header.stamp = msg->header.stamp;
+        cloud_msg.header.frame_id = output_lidar_frame2;
+        m_lidar_pub_2->publish(cloud_msg);
     }
 
     pcl::PointCloud<pcl::PointXYZINormal>::Ptr livox2PCL(const livox_ros_driver2::msg::CustomMsg::SharedPtr msg,
@@ -77,12 +102,19 @@ private:
         return cloud;
     }
 
-    rclcpp::Subscription<livox_ros_driver2::msg::CustomMsg>::SharedPtr m_lidar_sub;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_lidar_pub;
+    rclcpp::Subscription<livox_ros_driver2::msg::CustomMsg>::SharedPtr m_lidar_sub_1;
+    rclcpp::Subscription<livox_ros_driver2::msg::CustomMsg>::SharedPtr m_lidar_sub_2;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_lidar_pub_1;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_lidar_pub_2;
 
 
-    std::string input_lidar_topic = "livox/lidar_192_168_1_200";
-    std::string output_lidar_frame = "livox_frame_pointcloud2";
+    std::string input_lidar_topic1 = "livox/lidar_192_168_1_200";
+    std::string input_lidar_topic2 = "livox/lidar_192_168_1_201";
+    std::string output_lidar_frame1 = "livox_frame_pointcloud2";
+    std::string output_lidar_frame2 = "livox_frame_pointcloud2";
+    std::string output_lidar_topic1 = "livox_pointcloud2";
+    std::string output_lidar_topic2 = "livox_pointcloud2";
+    int lidar_num = 1;
     int filter_num = 3;
     double lidar_max_range = 20.0;
     double lidar_min_range = 0.0;
