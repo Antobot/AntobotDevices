c361ec5e1637d5336fed6452431bd63885db4af9
Daniel Freer <daniel.freer@antobot.ai>
2025-10-24 14:22:02 +0800

    Fixing lidarManagerClient

--- antobot_devices_lidar/antobot_devices_lidar/lidarManagerClient.py
+++ antobot_devices_lidar/antobot_devices_lidar/lidarManagerClient.py
@@ -5,7 +5,9 @@ from antobot_devices_msgs.srv import LidarManager
 class lidarManagerClient():
     '''Client for controllig lidar'''
 
-    def __init__(self, node_in=None):    
+    def __init__(self, node_in=None, need_response=False):    
+        
+        self.node_in = node_in
         
         self.serviceName = '/antobot/lidarManager'
         self.lidarManagerClient = node_in.create_client(LidarManager, self.serviceName)
@@ -22,16 +24,18 @@ class lidarManagerClient():
     def checkForService(self):
         return self.lidarManagerClient.service_is_ready()
 
-    def updateLidar(self):
+    def updateLidar(self, callback):
         
+        while not self.lidarManagerClient.wait_for_service(timeout_sec=1.0):
+            self.node_in.get_logger().info(f'SW2322: LiDAR Manager: Service {self.serviceName} not available, waiting again...')
+
         self.req.front_power_on = self.frontPowerOn
         self.req.front_costmap_enable = self.frontCostmapEnable
         self.req.rear_power_on = self.rearPowerOn
         self.req.rear_costmap_enable = self.rearCostmapEnable
 
-        lmResponse = self.lidarManagerClient.call_async(self.req)
+        self.lidarManagerClient.call_async(self.req).add_done_callback(callback)
 
-        print('Lidar Manager Client: '+ lmResponse.responseString)
-        return lmResponse
+        return
 
 
